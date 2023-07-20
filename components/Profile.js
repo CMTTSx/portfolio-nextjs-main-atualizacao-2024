290201d4836645cbe867b52b916f776da2c15c23
Calil Sousa Mattos <contatocsmpm@gmail.com>
2023-07-20 11:52:43 -0300

    update-dev-p-margin

--- components/Profile.js
+++ components/Profile.js
@@ -12,7 +12,7 @@ export default function Profile() {
       </div>
       <h1 className='flex justify-center sm:text-[28px] sm:mt-5 text-6xl xl:text-6xl xl:ml-5 xl:mt-[-1em] font-[700] mt-[-1em] mb-5 lg:mr-[6em] lg:mt-[-2em] md:text-4xl md:mt-[1em] md:mr-[-0.2em] text-[rgba(83,218,0,0.9)] ' >CALIL SOUSA MATTOS</h1>
       <h2 className='flex justify-center text-4xl mr-[9em] sm:ml-[1em] sm:text-[21px] xl:mr-[4em] xl:text-[2em] sm:mr-10 text-justify font-italic lg:ml-[-7em] lg:text-2xl md:ml-[2em]' >Desenvolvedor Fullstack Web e Mobile</h2>
-      <p className='flex text-justify text-lg mt-10 ml-10 mr-10 mb-20 font-[italic] xxl:ml-[25em]  ' >“Desenvolvedor que atua de ponta-a-ponta desde o Front-End, Banco de Dados, Back-End e Mobile. Sempre alinhando os estudos de novas tecnologias, vindo a aplicar todos os conceitos aprendidos em seus sistemas e aplicações. ”</p>
+      <p className='flex text-justify text-lg mt-10 ml-10 mr-10 mb-20 font-[italic] xl:ml-[6em]  ' >“Desenvolvedor que atua de ponta-a-ponta desde o Front-End, Banco de Dados, Back-End e Mobile. Sempre alinhando os estudos de novas tecnologias, vindo a aplicar todos os conceitos aprendidos em seus sistemas e aplicações. ”</p>
 
     </div>
   );
