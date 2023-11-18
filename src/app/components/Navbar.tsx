2684b0bc9936f7dbceed1699e698fe3e20a1265e
CSM <contatocsmpm@gmail.com>
2023-11-18 13:32:45 -0300

    navbar-component-add-go-to-function

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
 
@@ -9,29 +10,303 @@ import contactIcon from 'public/images/contact-Icon.svg';
 
 function Navbar() {
 
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })
+
+    };
+
+    const goToServices = () => {
+        window.scrollTo({
+            top: 950,
+            behavior: "smooth",
+        })
+
+        const mediaQuery = window.matchMedia('(width: 320px)')
+        // Check if the media query is true
+        if (mediaQuery.matches) {
+            window.scrollTo({
+                top: 1000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery360s = window.matchMedia('(width: 360px)')
+        // Check if the media query is true
+        if (mediaQuery360s.matches) {
+            window.scrollTo({
+                top: 1000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery375s = window.matchMedia('(width: 375px)')
+        // Check if the media query is true
+        if (mediaQuery375s.matches) {
+            window.scrollTo({
+                top: 950,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery390s = window.matchMedia('(width: 390px)')
+        // Check if the media query is true
+        if (mediaQuery390s.matches) {
+            window.scrollTo({
+                top: 950,
+                behavior: "smooth",
+            })
+        }
+        
+        const mediaQuery411s = window.matchMedia('(width: 411px)')
+        // Check if the media query is true
+        if (mediaQuery411s.matches) {
+            window.scrollTo({
+                top: 950,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery768s = window.matchMedia('(width: 768px)')
+        // Check if the media query is true
+        if (mediaQuery768s.matches) {
+            window.scrollTo({
+                top: 950,
+                behavior: "smooth",
+            })
+        }
+    };
+
+    const goToSkills = () => {
+        window.scrollTo({
+            top: 2530,
+            behavior: "smooth",
+        })
+
+        const mediaQuery = window.matchMedia('(width: 320px)')
+        // Check if the media query is true
+        if (mediaQuery.matches) {
+            window.scrollTo({
+                top: 5730,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery360s = window.matchMedia('(width: 360px)')
+        // Check if the media query is true
+        if (mediaQuery360s.matches) {
+            window.scrollTo({
+                top: 5720,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery375s = window.matchMedia('(width: 375px)')
+        // Check if the media query is true
+        if (mediaQuery375s.matches) {
+            window.scrollTo({
+                top: 5700,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery390s = window.matchMedia('(width: 390px)')
+        // Check if the media query is true
+        if (mediaQuery390s.matches) {
+            window.scrollTo({
+                top: 5650,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery411s = window.matchMedia('(width: 411px)')
+        // Check if the media query is true
+        if (mediaQuery411s.matches) {
+            window.scrollTo({
+                top: 5650,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery768s = window.matchMedia('(width: 768px)')
+        // Check if the media query is true
+        if (mediaQuery768s.matches) {
+            window.scrollTo({
+                top: 5000,
+                behavior: "smooth",
+            })
+        }
+
+
+    };
+
+    const goToPortfolio = () => {
+        window.scrollTo({
+            top: 5630,
+            behavior: "smooth",
+        })
+
+        const mediaQuery = window.matchMedia('(width: 320px)')
+        // Check if the media query is true
+        if (mediaQuery.matches) {
+            window.scrollTo({
+                top: 18100,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery360p = window.matchMedia('(width: 360px)')
+        // Check if the media query is true
+        if (mediaQuery360p.matches) {
+            window.scrollTo({
+                top: 18050,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery375p = window.matchMedia('(width: 375px)')
+        // Check if the media query is true
+        if (mediaQuery375p.matches) {
+            window.scrollTo({
+                top: 18050,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery390p = window.matchMedia('(width: 390px)')
+        // Check if the media query is true
+        if (mediaQuery390p.matches) {
+            window.scrollTo({
+                top: 18000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery411p = window.matchMedia('(width: 411px)')
+        // Check if the media query is true
+        if (mediaQuery411p.matches) {
+            window.scrollTo({
+                top: 17900,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery768p = window.matchMedia('(width: 768px)')
+        // Check if the media query is true
+        if (mediaQuery768p.matches) {
+            window.scrollTo({
+                top: 10200,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery1024p = window.matchMedia('(width: 1024px)')
+        // Check if the media query is true
+        if (mediaQuery1024p.matches) {
+            window.scrollTo({
+                top: 7300,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery1280p = window.matchMedia('(width: 1280px)')
+        // Check if the media query is true
+        if (mediaQuery1280p.matches) {
+            window.scrollTo({
+                top: 6450,
+                behavior: "smooth",
+            })
+        }
+    };
+
+    const goToContact = () => {
+        window.scrollTo({
+            top: 10000,
+            behavior: "smooth",
+
+        })
+
+        const mediaQuery = window.matchMedia('(width: 320px)')
+        // Check if the media query is true
+        if (mediaQuery.matches) {
+            window.scrollTo({
+                top: 21000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery360c = window.matchMedia('(width: 360px)')
+        // Check if the media query is true
+        if (mediaQuery360c.matches) {
+            window.scrollTo({
+                top: 25000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery375c = window.matchMedia('(width: 375px)')
+        // Check if the media query is true
+        if (mediaQuery375c.matches) {
+            window.scrollTo({
+                top: 25000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery390c = window.matchMedia('(width: 390px)')
+        // Check if the media query is true
+        if (mediaQuery390c.matches) {
+            window.scrollTo({
+                top: 25000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery411c = window.matchMedia('(width: 411px)')
+        // Check if the media query is true
+        if (mediaQuery411c.matches) {
+            window.scrollTo({
+                top: 25000,
+                behavior: "smooth",
+            })
+        }
+
+        const mediaQuery768c = window.matchMedia('(width: 768px)')
+        // Check if the media query is true
+        if (mediaQuery768c.matches) {
+            window.scrollTo({
+                top: 25000,
+                behavior: "smooth",
+            })
+        }
+
+    };
     return (
         <div className=' overflow-x-hidden ' >
 
             <aside className="h-full w-[4rem] flex flex-col space-y-10 items-center justify-center bg-[#9BFF06] text-white fixed ">
-                <div title='Perfil' className=" h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+
+                <div onClick={goToTop} title='Perfil' className=" h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" /></svg>
                 </div>
 
-                <div title='Serviços' className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+                <div onClick={goToServices} title='Serviços' className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" /></svg>
                 </div>
 
-                <div title='Habilidades' className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+                <div onClick={goToSkills} title='Habilidades' className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                     </svg>
                 </div>
 
-                <div title='Portfolio' className="h-8 w-8 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+                <div onClick={goToPortfolio} title='Portfolio' className="h-8 w-8 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                     <Image src={portfolioSVG} alt='portfolioSVG' />
                 </div>
 
-                <div title='Contato' className="h-8 w-8 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
+                <div onClick={goToContact} title='Contato' className="h-8 w-8 flex items-center justify-center rounded-lg cursor-pointer hover:text-white hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
                     <Image src={contactIcon} alt='contactIcon' />
                 </div>
             </aside>
