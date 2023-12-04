3b298b39d5248e2d685f2ba0a1f2f57de86ee788
CSM <contatocsmpm@gmail.com>
2023-12-04 15:59:58 -0300

    update-manutencao-page

--- src/app/components/FixSiteImage.tsx
+++ src/app/components/FixSiteImage.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 
-import Image1 from '../../../public/images/Atualizada-de-Sites-e-Manutenao-de-Sites-MarketingWeb.webp'
+import Image1 from '../../../public/images/manutencao-tchauzinho.gif'
 
 function FixSiteImage() {
   return (
-    <div>
-        <Image src={Image1} alt="Fix Site Image" className='mx-auto' />
+    <div className=' w-full ' >
+        <Image src={Image1} alt="Fix Site Image" className='mx-auto My-auto w-[50rem] ' />
     </div>
   )
 }
